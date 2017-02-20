4852f4a7da6af49d9aed9b60efcd16aa890472f1
barttermorshuizen <bart@moreawesome.co>
2017-02-20 13:40:01 +0100

    initial

--- referral-ios/ViewController.swift
+++ referral-ios/ViewController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var contactPicker: UIPickerView!
+    var pickerData: [String] = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // Input data into the Array:
+        pickerData = ["E-Mail", "Telefonisch"]
+        self.contactPicker.delegate = self
+        self.contactPicker.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // The number of columns of data
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
 }
 
